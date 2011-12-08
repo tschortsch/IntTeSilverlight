be0aaf6647c810d809a90f38777759055354cabb
Juerg Hunziker <juerg.hunziker@gmail.com>
2011-12-08 09:14:56 +0100

    started implementing gameservice

---
+++ IntTeTestat/Game.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using IntTeTestat.ViewModel;
+
+namespace IntTeTestat
+{
+    public partial class PickName : UserControl
+    {
+        public PickName()
+        {
+            InitializeComponent();
+            DataContext = new GameModel();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            WebContext.Current.GuessServiceClient.AddNameAsync(nameTextBox.Text);
+        }
+    }
+}
